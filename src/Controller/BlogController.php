c2ff3d69aa99f62bea27d7bbde9a8a8d8c6ceaf8
giroz <melanie>
2019-05-14 11:58:41 +0200

    quete 7

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,47 +2,99 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
+    
     /**
+     *
      * @Route("/blog", name="blog_index")
+     * @return Response A response instance
      */
-    public function index()
+    public function index(): Response
     {
-        return $this->render('blog/index.html.twig', [
-            'owner' => 'Mélanie',
-        ]);
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        if (!$articles) {
+            throw $this->createNotFoundException(
+            'No article found in article\'s table.'
+            );
+        }
+
+        return $this->render(
+                'blog/index.html.twig',
+                ['articles' => $articles]
+        );
     }
 
     /**
-     * @Route("/blog/show/{page}", name="blog_show", requirements={"page"="^[a-z0-9]+(?:-[a-z0-9]+)*$"}, methods={"GET"}, defaults={"page"="/"})
-     * 
+     * Getting a article with a formatted page for title
+     *
+     * @param string $page The pageger
+     *
+     * @Route("/blog/{page<^[a-z0-9-]+$>}",
+     *     defaults={"page" = null},
+     *     name="blog_show")
+     *  @return Response A response instance
      */
-    public function show($page)
+    public function show(string $page) : Response
     {
-        
-        
-        if ($page == "/") {
-            $page = "article sans titre";
-            $page = ucwords($page);
-            return $this->render('blog/show.html.twig', [
-                'page' => $page,
-                
-            ]);
-        }else{
-            $traitmentTiret = explode("-", $page);
-            $chaine = implode(" ",$traitmentTiret );
-            $page = ucwords($chaine);
+        if (!$page) {
+                throw $this
+                ->createNotFoundException('No page has been sent to find an article in article\'s table.');
+            }
 
-            return $this->render('blog/show.html.twig', [
-                'page' => $page,
-                'traitmentTiret' => $traitmentTiret,
-                'chaine' => $chaine
-            ]);
+        $page = preg_replace('/-/',' ', ucwords(trim(strip_tags($page)), "-"));
+
+        $article = $this->getDoctrine()
+                ->getRepository(Article::class)
+                ->findOneBy(['title' => mb_strtolower($page)]);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+            'No article with '.$page.' title, found in article\'s table.'
+        );
         }
+
+        return $this->render(
+        'blog/show.html.twig',
+        [
+                'article' => $article,
+                'page' => $page,
+        ]
+        );
+    }
+
+    /**
+     * @Route("blog/category/{category}", name="show_category", defaults={"category" = null})
+     */
+    public function showByCategory(string $category)
+    {
+        $category = $this->getDoctrine()
+                ->getRepository(Category::class)
+                ->findOneByName($category);
+        $article = $this->getDoctrine()
+                ->getRepository(Article::class)
+                ->findByCategory($category,
+                 ['id' => 'DESC'],3);
+               
+        
+       
+
+        return $this->render(
+        'blog/category.html.twig',[
+            'category' => $category,
+            'articles' => $article
+        
+            
+            ]
+        );
     }
 }
