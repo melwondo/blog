54b214cdc01ef51e65b3a1b8b4d42b624e25a16c
giroz <melanie>
2019-05-20 17:21:54 +0200

    quete 11

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -75,4 +75,11 @@ class Tag
 
         return $this;
     }
+
+    public function __toString(){
+        // to show the name of the Category in the select
+        return $this->name;
+        // to show the id of the Category in the select
+        // return $this->id;
+    }
 }
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -2,26 +2,27 @@
 
 namespace App\Form;
 
-use App\Entity\Category;
+use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 
-
-class CategoryType extends AbstractType
+class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('title')
+            ->add('content')
+            ->add('category', null, ['choice_label' => 'name'])
+            ->add('tags')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Category::class,
+            'data_class' => Article::class,
         ]);
     }
 }
