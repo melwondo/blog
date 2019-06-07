f7639d19a21b5973a9c1e6e1a839ca3edcd44b85
giroz <melanie>
2019-06-07 15:09:30 +0200

    swiftmailer

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -29,7 +29,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/new", name="article_new", methods={"GET","POST"})
      */
-    public function new(Request $request, Slugify $slugify): Response
+    public function new(Request $request, Slugify $slugify, \Swift_Mailer $mailer): Response
     {
         $article = new Article();
         $form = $this->createForm(ArticleType::class, $article);
@@ -41,6 +41,17 @@ class ArticleController extends AbstractController
             $article->setSlug($slugify->generate($article->getTitle()));
             $entityManager->persist($article);
             $entityManager->flush();
+
+            $message = (new \Swift_Message('Article :' . $article->getTitle()))
+            ->setFrom($this->getParameter('mailer_from', 'sendmail'))
+            ->setTo('melanie.giroz2@gmail.com')
+            ->setBody($this->renderView(
+                'article/email/notification.html.twig',
+                ['article' => $article]
+            ),
+            'text/html');
+            
+            $mailer->send($message);
 
             return $this->redirectToRoute('article_index');
         }
